8ea08866accfff8199dec81cea47f0f44c50f780
Shahad Bakoos <shahadbakoos@gmail.com>
2019-02-06 15:25:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function justInvoke(fn) {
+  return fn()
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg)
+
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args)
+
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  let x = functionToBeCopied.bind(thisValue)
+  return x
+
+}
